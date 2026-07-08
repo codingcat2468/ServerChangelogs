81923ac57b9911ed0133a22d2fb0ca601a7704b9
codingcat2468 <lw-app-produktion@gmx.net>
2026-07-08 03:39:24 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java-library")
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("xyz.jpenilla.resource-factory-paper-convention") version "1.3.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+}
+
+paperPluginYaml {
+    main = "com.codingcat.ServerChangelogs"
+    apiVersion = "26.1.2"
+
+    authors.addAll("codingcat2468")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("26.1.2")
+        jvmArgs("-Xms2G", "-Xmx2G")
+    }
+}

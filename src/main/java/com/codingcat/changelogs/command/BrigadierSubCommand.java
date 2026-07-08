63a385d6ce0ceda6a1df745379eaee2417224d6c
codingcat2468 <lw-app-produktion@gmx.net>
2026-07-08 15:09:50 +0200

    Add a new "create" sub-command to open the CreateChangelogDialog

--- src/main/java/com/codingcat/changelogs/command/BrigadierSubCommand.java
+++ src/main/java/com/codingcat/changelogs/command/BrigadierSubCommand.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Set;
 
 public interface BrigadierSubCommand {
-    @NotNull Set<BrigadierSubCommand> COMMANDS = Set.of(new ReloadSubCommand());
+    @NotNull Set<BrigadierSubCommand> COMMANDS = Set.of(new CreateChangelogSubCommand(), new ReloadSubCommand());
 
     @NotNull LiteralCommandNode<CommandSourceStack> build(@NotNull ServerChangelogs plugin);
 }
